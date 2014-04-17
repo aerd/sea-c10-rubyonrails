5b3196bb24168d6d1b7124bfcbc911870d9ffa22
Andrea Nelson <aerd@live.com>
2014-04-16 21:02:48 -0700

    submitting homework

---
+++ week2_homework/calculator.rb
@@ -0,0 +1,43 @@
+class Calculator
+
+  #attr_accessor :sum
+
+  def  sum numbers
+  	total = 0
+  	numbers.each do |i|
+  		total += i
+  	end
+  	return total
+
+  end
+
+
+  def multiply *numbers
+
+  	total = 1
+  	numbers.flatten.each do |i|
+  		total *= i
+  	end
+  	return total
+
+  end
+
+  def pow (x,y)
+  	total = x**y
+  	return total
+
+  end
+
+
+  def fac (x)
+  	total = 1
+	  	while x > 0 do
+	  		total = total * x
+	  		x -= 1 
+	  		
+	 		end
+	return total
+	
+	end
+
+end
