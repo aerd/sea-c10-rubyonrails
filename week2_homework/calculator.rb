f9d2ef0da9d576690794f9e6102c300ca38cec58
Andrea Nelson <aerd@live.com>
2014-04-17 21:11:50 -0700

    fixing spaces yet again

--- week2_homework/calculator.rb
+++ week2_homework/calculator.rb
@@ -1,33 +1,32 @@
 class Calculator
 
   def  sum numbers
-  	total = 0
-  	numbers.each do |i|
-  	 total += i
-  	end
-  	return total
+    total = 0
+    numbers.each do |i|
+  	  total += i
+    end
+    return total
   end
 
   def multiply *numbers
-  	total = 1
-  	numbers.flatten.each do |i|
-  		total *= i
-  	end
-  	return total
+    total = 1
+    numbers.flatten.each do |i|
+  	  total *= i
+    end
+    return total
   end
 
   def pow (x,y)
-  	total = x**y
-  	return total
+    total = x**y
+    return total
   end
 
   def fac (x)
-  	total = 1
-	  	while x > 0 do
-	  		total = total * x
-	  		x -= 1 
-	  	end
+    total = 1
+	    while x > 0 do
+	  	  total = total * x
+	  	  x -= 1 
+	    end
     return total
-	end
-
+  end
 end
