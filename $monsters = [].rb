2a1ea4315fca79da223e221ef33e63049158b4e6
Andrea Nelson <aerd@live.com>
2014-04-23 18:58:12 -0700

    homework - Monsters

---
+++ $monsters = [].rb
@@ -0,0 +1,55 @@
+$monsters = []
+
+$monsters << {
+  :name => 'Zombie',
+  :nocturnal => false,
+  :dangers => ['bites', 'scratches'],
+  :vulnerabilities => ['fire', 'decapitation'],
+  :legs => 2
+}
+$monsters << {
+  :name => 'Mummy',
+  :nocturnal => false,
+  :dangers => ['bites', 'scratches', 'curses'],
+  :vulnerabilities => ['fire', 'decapitation', 'cats'],
+  :legs => 2
+}
+$monsters << {
+  :name => 'Vampire',
+  :nocturnal => true,
+  :dangers => ['bites', 'hypnosis'],
+  :vulnerabilities => ['wood', 'decapitation', 'crosses', 'holy_water', 'garlic', 'daylight'],
+  :legs => 2
+}
+$monsters << {
+  :name => 'Werewolf',
+  :nocturnal => true,
+  :dangers => ['bites', 'scratches'],
+  :vulnerabilities => ['silver'],
+  :legs => 4
+}
+$monsters << {
+  :name => 'Blob',
+  :nocturnal => false,
+  :dangers => ['suffocation'],
+  :vulnerabilities => ['CO2', 'ice', 'cold'],
+  :legs => 0
+}
+
+puts "How many monsters are two legged?"
+#puts $monsters.select{|m| m[:legs] == 2 }
+puts $monsters.count{ |m| m[:legs] == 2 }
+#puts $monsters.select{|m| m[:legs]== 2}.map{|m| m[:name]}
+
+puts "How many monsters are nocturnal?"
+puts $monsters.count{|m| m[:nocturnal] == true }
+puts "What are the names of the monsters that are nocturnal?"
+puts $monsters.select{|m| m[:nocturnal]== true}.map{|m| m[:name]}
+puts "How many legs do all our monsters have?"
+puts $monsters.map {|m| m[:legs]}.inject (:+)
+puts "What are the 2 most common dangers of our monsters?"
+puts $monsters.map {|m| m[:dangers]}.max_by(&:size).first 2
+puts "What are the 2 most common vulnerabilities of our monsters?"
+puts $monsters.map {|m| m[:vulnerabilities]}.max_by(&:size).first 2
+
+
